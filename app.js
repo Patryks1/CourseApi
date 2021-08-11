60a2cb816f1f1274888b9ad86cac2988ca7cb0d9
PatrykS <slowinskipatryk1321@gmail.com>
2021-08-11 18:16:37 +0100

    Docker

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import app from './server';
 
-const mongoDB = process.env.MONGO_DB || 'mongodb://localhost:27018/coursesApi';
+const mongoDB = process.env.MONGO_DB || 'mongodb://localhost:27017/coursesApi';
 const port = process.env.PORT || 3000;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true }).then(async () => {
