311331e6f87da4130114e1a8bba0d2dee3fef1d3
PatrykS <slowinskipatryk1321@gmail.com>
2021-08-13 00:06:09 +0100

    Validation and dictionary refactor

---
+++ dictionary/courses.dictionary.js
@@ -0,0 +1,13 @@
+ const dictionary = {
+  missingRequiredData: 'Missing required data',
+  invalidCourseOrSessionId: 'Invalid courseId or sessionId',
+  invalidCourseId: 'Invalid courseId',
+  courseIdDoesNotExist: 'courseId does not exist',
+  courseIdoOrSessionIdDoesNotExist: 'courseId or sessionId does not exist',
+  saved: 'Saved!',
+  failedToSaveInDatabase: 'Failed to save session in database',
+  recordAlreadyExists: 'sessionId already exists',
+  courseDoesNotExist: 'Course does not exist',
+};
+
+export default dictionary;
---
+++ dictionary/index.js
@@ -0,0 +1,1 @@
+export { default as dictionary } from './courses.dictionary';
